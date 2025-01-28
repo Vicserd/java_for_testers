e286aa56214308005c70e68c0e9cb1a9238ac6c4
Виктор С. <Vicserd09@yandex.ru>
2025-01-28 23:15:09 +0400

    Доработана функция equals , добавлены тесты

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -21,7 +21,7 @@ public class TriangleTests {
     void canCalculatePerimeterTriangle() {
     Assertions.assertEquals(12.0,  new Triangle(3.0,4.0,5.0).perimeter());
     }
-*/
+
     @Test
     void cannotCreateTriangleWithNegativeSide_a(){
         try {
@@ -62,6 +62,38 @@ public class TriangleTests {
            System.out.println(exception);
        };
    }
+*/
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0, 3.0, 3.0);
+        var t2 = new Triangle(3.0, 3.0, 3.0);
 
+        Assertions.assertEquals(t1, t2);
+    }
+    @Test
+    void testEquality2(){
+        var t1 = new Triangle(5.0,4.0, 3.0);
+        var t2 = new Triangle(5.0,3.0, 4.0);
+        var t3 = new Triangle(4.0,5.0, 3.0);
+        var t4 = new Triangle(4.0,3.0, 5.0);
+        var t5 = new Triangle(3.0,4.0, 5.0);
+        var t6 = new Triangle(3.0,5.0, 4.0);
+
+        Assertions.assertEquals(t1,t2);
+        Assertions.assertEquals(t3,t4);
+        Assertions.assertEquals(t5,t6);
+        Assertions.assertEquals(t1,t3);
+        Assertions.assertEquals(t1,t4);
+        Assertions.assertEquals(t1,t5);
+        Assertions.assertEquals(t1,t6);
+        Assertions.assertEquals(t2,t3);
+        Assertions.assertEquals(t2,t4);
+        Assertions.assertEquals(t2,t5);
+        Assertions.assertEquals(t2,t6);
+        Assertions.assertEquals(t3,t5);
+        Assertions.assertEquals(t3,t6);
+        Assertions.assertEquals(t4,t5);
+        Assertions.assertEquals(t4,t6); //будет ли правильным такой перебор или же на каждый перебор нужен отдельный тест?
+    }
 }
 
