7ebb28f3a7ffe8f8ace22945f5ea2252b768f559
Виктор С. <Vicserd09@yandex.ru>
2025-03-10 23:37:46 +0400

    5.5-5.7 File, YAML, XML

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
@@ -4,10 +4,13 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -34,6 +37,7 @@ public class Generator {
         generator.run();
 
     }
+
     private void run() throws IOException {
         var data = generate();
         save(data);
@@ -41,10 +45,22 @@ public class Generator {
     }
 
     private void save(Object data) throws IOException {
-        if ("json".equals(format)){
+        if ("json".equals(format)) {
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            var json = mapper.writeValueAsString(data);
+
+            try (var writer = new FileWriter(output)) {
+                writer.write(json);
+            }
+
+        } if ("yaml".equals(format)) {
+            var mapper = new YAMLMapper();
             mapper.writeValue(new File(output), data);
+        } if ("xml".equals(format)) {
+            var mapper = new XmlMapper();
+            mapper.writeValue(new File(output), data);
+
         } else {
             throw new IllegalArgumentException("Неизвестный формат данных" + format);
         }
