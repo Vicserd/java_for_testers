40306114eb15e2949abd4d3e2e2fc3ad03b289dc
Виктор С. <Vicserd09@yandex.ru>
2025-03-11 23:53:45 +0400

    ДЗ13 - Properties

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/ApplicationManager.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     protected WebDriver driver;
 
@@ -16,7 +18,11 @@ public class ApplicationManager {
 
     private ContactHelper contact;
 
-    public void init(String browser) {
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
+
         if (driver == null) {
             if ("firefox".equals(browser)){
                 driver = new FirefoxDriver();
@@ -26,9 +32,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1280, 1024));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
